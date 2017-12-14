83c9839401b377b7f6593b6083c56c67f53ec982
Amy Carney <amy.carney@alaska.gov>
2017-12-14 18:44:52 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,11 +9,5 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
-  taken = nil
-  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-    taken = false
-  else
-    taken = true
-  end
-  taken
+  board[index] != " "
 end
